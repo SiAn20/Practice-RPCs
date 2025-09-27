c3e343986325c54fd06bb5a9f445e54d790fc10c
SiAn20 <sidneyangellysg@gmail.com>
2025-09-27 17:22:27 -0400

    feat(Sep_27_2025): add problem solutions for A_Intentional, B_Full, and C_Duck

---
+++ Sep_27_2025/C_Duck.cpp
@@ -0,0 +1,39 @@
+/*
+  author: SiAn20
+  Name: Sidney Angelly Sahonero Garrado
+*/
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long
+#define endl '\n'
+
+void solve() {
+  int n, total = 0, mayor = 0;
+  cin >> n;
+  vector<int> a(n);
+
+  for (int &x : a) {
+    cin >> x;
+    total += x;
+    mayor = max(mayor, x);
+  }
+  total = total / 3;
+  if (mayor > total)
+    cout << "NO" << endl;
+  else
+    cout << "YES" << endl;
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  solve();
+}
+
+/*
+     /\_/\
+    ( o.o )
+     > ^ <
+*/
